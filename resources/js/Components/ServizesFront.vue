d731003588e22439f56e2d05b31e4ed9c1651432
Marin Nikolli <sibalonat@gmail.com>
2023-12-14 23:50:57 +0100

    .

--- resources/js/Components/ServizesFront.vue
+++ resources/js/Components/ServizesFront.vue
@@ -50,25 +50,8 @@ const loading = (comp, evt) => {
 onMounted(() => {})
 </script>
 <template>
-        <!-- <div class="relative w-full border border-solid rounded-md text-gray-forth">
-            <img :src="comp.media" v-if="comp.media" class="object-cover w-full h-44" alt="">
-            <p class="relative z-40 px-5 py-1 -mt-8 border-t border-b bg-first-gray w-fit text-start">
-                {{ comp.area }}
-            </p>
-            <div class="relative grid w-full h-full grid-cols-6 p-10 gap-x-5">
-                <div class="col-span-6">
-                    <p class="pb-8 font-sans text-sm wrapper">
-                        {{ comp.short_description }}
-                    </p>
-                    <p class="pb-0 mt-2 text-xl uppercase cursor-pointer font-display"
-                    @click="loading(comp, $event)">
-                        {{ comp.title }}
-                    </p>
-                </div>
-            </div>
-        </div> -->
         <div class="relative w-full border-2 border-solid rounded-md min-h-44 text-gray-forth border-mbyll">
-            <img :src="comp.media" v-if="comp.media" class="absolute object-cover w-full h-44" alt="">
+            <img :src="comp.media" v-if="comp.media" class="absolute object-cover w-full h-full" alt="">
             <div class="relative z-40 w-2/3 bg-slight">
                 <p class="relative px-5 py-1 border-t border-b bg-first-gray w-fit text-start">
                     {{ comp.area }}
